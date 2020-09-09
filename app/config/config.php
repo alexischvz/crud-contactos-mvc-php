b58441bf357546db1c3d1624b9fa3a7c1c24268d
alexischavezweb <alexischavezweb@gmail.com>
2020-09-08 21:30:57 -0600

    Configuracion de base de datos

---
+++ app/config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'contactos_mvc');
+
+/* *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*= */
+
+define('RUTA_APP', dirname(dirname(__FILE__)));
+
+define('RUTA_URL', 'http://localhost/crud-contactos-mvc');
+
+define('TITLE', 'Contactos');
